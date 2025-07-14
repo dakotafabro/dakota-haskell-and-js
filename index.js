ff8cda9e91056a20e9260ea7ac7ac913ac952199
dakotafabro <dakotafabrocgu@gmail.com>
2025-07-14 11:39:14 -0700

    create greeting messages

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 import { greet } from "./lib/greet.js";
 
+//* Things to remember
+// use const & Object.freeze() for immutability
+// keep functions pure (no side effects)
+// okay and expected to use docs and online resources
+// ?? Appropriate uses for AI during this exercise?
+// assign types in var names and initial values
+// use functions to return an object (avoid classes)
+// lean on higher order functions - map(), reduce(), filter()
+// abstraction when necessary
+// handle null values
+// demonstrate & verbalize thought process
+// */
+
 //* TODO
 //
 //
 // */
 
-console.log(greet("Hal, Jared, & Dakota"));
+console.group("Greetings");
+console.log(greet("Hal"));
+console.log(greet("Jared"));
+console.log(greet("Dakota"));
+console.groupEnd();
